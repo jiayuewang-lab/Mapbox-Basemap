6e45e751727c0cb8333fce3096078cc02314ce6f
jiayuewang-lab <jiayuewang@berkeley.edu>
2025-10-28 23:20:15 -0400

    Add files via upload
    Add Mapbox Basemap project files

---
+++ Script.js
@@ -0,0 +1,8 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoiamlheXUxMjMiLCJhIjoiY21oYmQycHk2MXhidDJ2c2R0a25uejN3bSJ9.aNYdPQGQtL7eHfOgpVIWzg';
+
+const map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/jiayu123/cmhbdjgrj004w01s15eb2hfqm',
+  center: [-122.27, 37.87],
+  zoom: 9
+});
